e6ed92fafca415f7f975c57de0dddff9efb5abd5
Michael Harpe <michael@runreveal.com>
2024-03-08 09:18:04 -0500

    updated onepassword unusual client

--- detections/onepassword/unusual_client.sql
+++ detections/onepassword/unusual_client.sql
@@ -1,4 +1,10 @@
-select * from one_password_logs
-where not isNull(`client.appName`) AND
-      `client.appName` NOT IN {clients:Array(String)}
+WITH previousClients as (select DISTINCT `actor.email` user,
+groupUniqArray(client.appName) clients from one_password_logs
+WHERE eventTime BETWEEN {from:DateTime} - INTERVAL 30 DAY AND {from:DateTime} AND not isNull(`client.appName`)
+GROUP BY user)
+
+select one_password_logs.*, previousClients.clients previousClients from one_password_logs
+LEFT OUTER JOIN previousClients ON previousClients.user = `actor.email`
+where not isNull(`client.appName`)
 AND receivedAt BETWEEN {from:DateTime} AND {to:DateTime}
+AND previousClients.user <> '' AND NOT has(previousClients.clients, one_password_logs.`client.appName`)
