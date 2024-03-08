4872b6825eedf57ff766746a9552f2ec2377cd0f
Michael Harpe <michael@runreveal.com>
2024-03-08 09:50:30 -0500

    Updated unusual client and added impossible travel.

--- detections/onepassword/unusual_client.sql
+++ detections/onepassword/unusual_client.sql
@@ -1,10 +1,11 @@
 WITH previousClients as (select DISTINCT `actor.email` user,
-groupUniqArray(client.appName) clients from one_password_logs
-WHERE eventTime BETWEEN {from:DateTime} - INTERVAL 30 DAY AND {from:DateTime} AND not isNull(`client.appName`)
+groupUniqArray(tuple(`client.appName`, `client.platformName`)) clients from one_password_logs
+WHERE eventTime BETWEEN {from:DateTime} - INTERVAL 30 DAY AND {from:DateTime} 
+AND not isNull(`client.appName`) AND client.appName <> '' AND client.platformName <> '' AND user NOT like '%1passwordserviceaccounts.com'
 GROUP BY user)
 
 select one_password_logs.*, previousClients.clients previousClients from one_password_logs
 LEFT OUTER JOIN previousClients ON previousClients.user = `actor.email`
-where not isNull(`client.appName`)
+where not isNull(`client.appName`) AND client.appName <> '' AND client.platformName <> '' AND user NOT like '%1passwordserviceaccounts.com'
 AND receivedAt BETWEEN {from:DateTime} AND {to:DateTime}
-AND previousClients.user <> '' AND NOT has(previousClients.clients, one_password_logs.`client.appName`)
+AND previousClients.user <> '' AND NOT has(previousClients.clients, tuple(`client.appName`, `client.platformName`))
