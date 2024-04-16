30f7910dd4c324be7f8773bda46b115b13e41d54
Evan Johnson <evan@runreveal.com>
2024-04-16 13:38:10 -0500

    Fix blank detection

--- detections/gcp/gcp-workforce-pool-change.sql
+++ detections/gcp/gcp-workforce-pool-change.sql
@@ -1,2 +1,6 @@
 SELECT * from gcp_logs
 where receivedAt > {from:DateTime} and receivedAt < {to:DateTime}
+and methodName in (
+    'google.iam.admin.v1.WorkforcePools.CreateWorkforcePool',
+    'google.iam.admin.v1.WorkforcePools.UpdateWorkforcePool'
+)
