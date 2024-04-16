e6bab8be72f186fe277894f68884b10558439839
Evan Johnson <evan@runreveal.com>
2024-04-15 20:39:17 -0500

    Typo during testing.

--- detections/gcp/gcp-gmail-added-to-project.sql
+++ detections/gcp/gcp-gmail-added-to-project.sql
@@ -2,6 +2,6 @@ SELECT * from gcp_logs
 where receivedAt > {from:DateTime} and receivedAt < {to:DateTime}
 AND methodName = 'SetIamPolicy' 
 AND arrayExists(
-    x -> JSONExtractString(x, 'member') NOT LIKE '%@gmail.com',
+    x -> JSONExtractString(x, 'member') LIKE '%@gmail.com',
     JSONExtractArrayRaw(rawLog, 'message', 'data', 'protoPayload', 'serviceData', 'policyDelta', 'bindingDeltas')
 )
