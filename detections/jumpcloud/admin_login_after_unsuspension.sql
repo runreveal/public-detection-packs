f63cd03896f6af33f8d19d4cc809371ef4cfe94b
Gustavo Silva <gustavo@runreveal.com>
2025-05-28 16:08:50 +0100

    fixup: eventTime -> receivedAt

--- detections/jumpcloud/admin_login_after_unsuspension.sql
+++ detections/jumpcloud/admin_login_after_unsuspension.sql
@@ -1,15 +1,15 @@
 WITH admin_unsuspension_events AS (
     SELECT
-        eventTime,
+        receivedAt,
         rawLog.initiated_by.username,
     FROM logs
     WHERE sourceType = 'jumpcloud' AND
           eventName = 'admin_unsuspended'
-          eventTime >= (eventTime >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
+          receivedAt >= (receivedAt >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
 ),
 admin_auth_events AS (
     SELECT
-        eventTime,
+        receivedAt,
         rawinitiated_by.username,
         eventName
     FROM logs
@@ -21,18 +21,18 @@ admin_auth_events AS (
             'admin_totp_disable',
             'admin_privilege_grant'
           ) AND
-          eventTime >= (eventTime >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
+          receivedAt >= (receivedAt >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
 )
 SELECT
-    ae.eventTime as admin_auth_time,
-    ue.eventTime as admin_unsuspension_time,
+    ae.receivedAt as admin_auth_time,
+    ue.receivedAt as admin_unsuspension_time,
     ae.admin_id,
     ae.username,
     ae.eventName as admin_auth_event_type,
-    dateDiff('minute', ue.eventTime, ae.eventTime) as minutes_after_unsuspension
+    dateDiff('minute', ue.receivedAt, ae.receivedAt) as minutes_after_unsuspension
 FROM admin_auth_events ae
 INNER JOIN admin_unsuspension_events ue
     ON ae.username = ue.username
-WHERE ae.eventTime > ue.eventTime
-  AND ae.eventTime <= ue.eventTime + INTERVAL 1 HOUR
-ORDER BY ae.eventTime DESC
+WHERE ae.receivedAt > ue.receivedAt
+  AND ae.receivedAt <= ue.receivedAt + INTERVAL 1 HOUR
+ORDER BY ae.receivedAt DESC
--- detections/jumpcloud/user_portal_login_after_unsuspension.sql
+++ detections/jumpcloud/user_portal_login_after_unsuspension.sql
@@ -1,16 +1,16 @@
 WITH unsuspension_events AS (
     SELECT
-        eventTime,
+        receivedAt,
         rawLog.initiated_by.username,
         sourceType
     FROM logs
     WHERE sourceType = 'jumpcloud' AND
           eventName = 'user_unsuspended' AND
-          eventTime >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
+          receivedAt >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
 ),
 login_events AS (
     SELECT
-        eventTime,
+        receivedAt,
         rawLog.initiated_by.username,
         eventName
         sourceType
@@ -22,17 +22,17 @@ login_events AS (
         'user_lockout',
         'user_suspended'
     ) AND
-          eventTime >= (eventTime >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
+          receivedAt >= (receivedAt >= {to:DateTime} - INTERVAL {interval:Int64} DAY)
 )
 SELECT
-    le.eventTime as login_time,
-    ue.eventTime as unsuspension_time,
+    le.receivedAt as login_time,
+    ue.receivedAt as unsuspension_time,
     le.username,
     le.eventName as login_event_type,
-    dateDiff('minute', ue.eventTime, le.eventTime) as minutes_after_unsuspension
+    dateDiff('minute', ue.receivedAt, le.receivedAt) as minutes_after_unsuspension
 FROM login_events le
 INNER JOIN unsuspension_events ue
     ON le.username = ue.username
-WHERE le.eventTime > ue.eventTime
-  AND le.eventTime <= ue.eventTime + INTERVAL 1 HOUR
-ORDER BY le.eventTime DESC
+WHERE le.receivedAt > ue.receivedAt
+  AND le.receivedAt <= ue.receivedAt + INTERVAL 1 HOUR
+ORDER BY le.receivedAt DESC
