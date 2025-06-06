ff0e737ada58de009bbe9d5c2c68bf987b062c88
Gustavo Silva <gustavo@runreveal.com>
2025-06-06 17:31:23 +0100

    fixup: cleanup multiple_admin_actions query to simply the logic

--- detections/1password/multiple_admin_actions.sql
+++ detections/1password/multiple_admin_actions.sql
@@ -1,35 +1,24 @@
-WITH adminActions AS (
-    SELECT
-        `actor.email` AS user,
-        actor_uuid,
-        eventName,
-        eventTime,
-        receivedAt,
-        COUNT(*) OVER (
-            PARTITION BY actor_uuid 
-            ORDER BY eventTime 
-            RANGE BETWEEN INTERVAL 5 MINUTE PRECEDING AND CURRENT ROW
-        ) AS actionsInWindow
-    FROM one_password_logs
-    WHERE 
-        eventName IN ('create', 'delete', 'update', 'grant', 'revoke', 'suspend', 'reactive')
-        AND eventTime >= ({from:DateTime} - toIntervalMinute(30))
-        AND receivedAt >= {from:DateTime}
-        AND receivedAt <= {to:DateTime}
-        AND `actor.email` NOT LIKE '%1passwordserviceaccounts.com'
-)
+WITH adminActions AS
+    (
+        SELECT
+            `actor.email` AS user,
+            eventName,
+            receivedAt,
+            COUNT(*) OVER (PARTITION BY user) AS actionsInWindow
+        FROM one_password_logs
+        WHERE (eventName IN ('create', 'delete', 'update', 'grant', 'revoke', 'suspend', 'activate', 'beginr', 'reactive')) AND (receivedAt > ({from:DateTime} - toIntervalMinute(120))) AND (receivedAt <= {to:DateTime}) AND (user NOT LIKE '%1passwordserviceaccounts.com')
+    )
 SELECT
     user,
-    actor_uuid,
     COUNT(*) AS totalActions,
     MAX(actionsInWindow) AS maxActionsInWindow,
-    MIN(eventTime) AS firstAction,
-    MAX(eventTime) AS lastAction,
-    MAX(receivedAt) AS receivedAt,
-    groupArray(DISTINCT eventName) AS actionTypes
+    MIN(receivedAt) AS firstAction,
+    MAX(receivedAt) AS lastAction,
+    groupArrayDistinct(eventName) AS actionTypes
 FROM adminActions
 WHERE actionsInWindow > 10
-GROUP BY user, actor_uuid
+GROUP BY user
 HAVING COUNT(*) > 10
-ORDER BY maxActionsInWindow DESC, totalActions DESC
-;
+ORDER BY
+    maxActionsInWindow DESC,
+    totalActions DESC
