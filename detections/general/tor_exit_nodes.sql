c1781e0fb41ed79e3facb05ffaa479a9b9a21cba
Gustavo Silva <gustavo@runreveal.com>
2025-07-29 16:49:45 +0100

    feat/tor_exit_nodes: include actor in returned data

--- detections/general/tor_exit_nodes.sql
+++ detections/general/tor_exit_nodes.sql
@@ -9,7 +9,8 @@ SELECT
     dstIP,
     dstCity,
     dstASCountryCode,
-    dstASOrganization
+    dstASOrganization,
+    actor
 FROM runreveal_logs
 INNER JOIN threat_feed_ip_list ON (runreveal_logs.srcIP = threat_feed_ip_list.ip) OR (runreveal_logs.dstIP = threat_feed_ip_list.ip)
 WHERE ((receivedAt >= {from:DateTime}) AND (receivedAt <= {to:DateTime})) AND (feedName = 'Tor Exit')
