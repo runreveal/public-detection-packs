15d6acf6c13194367688a27e088f123eef3291a6
Michael Harpe <michael@runreveal.com>
2026-01-21 09:15:20 -0500

    Remove the double single quotes

--- detections/health-checks/health-check-no-logs.sql
+++ detections/health-checks/health-check-no-logs.sql
@@ -4,5 +4,5 @@ SELECT
     max(timestamp) as lastReceived
 FROM external_metrics
 WHERE timestamp >= now() - INTERVAL {duration:UInt64} HOUR
-  AND series = ''events_by_source''
-  AND tags[''sourceid''] = {sourceID:String}
+  AND series = 'events_by_source'
+  AND tags['sourceid'] = {sourceID:String}
