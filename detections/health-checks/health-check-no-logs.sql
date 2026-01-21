d06e2442af65c72ff923415a7c6cadb38d36aa98
Michael Harpe <michael@runreveal.com>
2026-01-21 09:06:46 -0500

    Add health check query

---
+++ detections/health-checks/health-check-no-logs.sql
@@ -0,0 +1,8 @@
+SELECT
+    {sourceID:String} as sourceID,
+    COALESCE(sum(value), 0) as eventCount,
+    max(timestamp) as lastReceived
+FROM external_metrics
+WHERE timestamp >= now() - INTERVAL {duration:UInt64} HOUR
+  AND series = ''events_by_source''
+  AND tags[''sourceid''] = {sourceID:String}
